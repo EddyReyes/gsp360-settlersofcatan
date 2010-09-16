1e054c07f0f6c79f287a4a8641233bd9268477b4
neurotoxin408 <neurotoxin408@d3192985-ef62-ff69-2b9f-3971aa3a1032>
2010-09-16 19:38:56 +0000

    made some updates to main.cpp

--- trunk/SettlersOfCatan/SettlersOfCatan/main.cpp
+++ trunk/SettlersOfCatan/SettlersOfCatan/main.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include "player.h"
 #include "resourceCards.h"
@@ -9,7 +8,7 @@ using namespace std;
 void OpeningMenu (int *);
 void AssignChitResources(char *);
 void DebugOutputResourceChits(char *);
-
+void RollDice(int *, int *);
 
 void main()
 {
@@ -24,19 +23,41 @@ void main()
 	AssignChitResources(&chits[12]);
 	//DebugOutputResourceChits(&chits[12]);
 
+	// Display the current status of the resource bank
+	ResourceDeck.DisplayResourceBankStatus();
+
 	// At this point we give player 2 three of each resource cards (for debugginb purposes)
 	playerTwo.drawResource(&ResourceDeck, WOOD, 3);
 	playerTwo.drawResource(&ResourceDeck, WHEAT, 3);
 	playerTwo.drawResource(&ResourceDeck, STONE, 3);
 	playerTwo.drawResource(&ResourceDeck, BRICK, 3);
 	playerTwo.drawResource(&ResourceDeck, SHEEP, 3);
+	cout << "Player two was given 3 resource cards of each type." << endl << endl;
 
-	//openingLayout();
-	//comeoutTurn();
-	while (winner == NULL)
-	{
-		//doNextTurn();
-	}
+	// Roll Dice
+	int Dice1, Dice2;
+	RollDice(&Dice1, &Dice2);
+	cout << "Player rolls dice: " << Dice1 << " and " << Dice2 << endl 
+		 << "Total: " << Dice1 + Dice2 << endl << endl;
+
+	// Display the current status of the resource bank
+	ResourceDeck.DisplayResourceBankStatus();
+
+	// Give the player a resouce according to value rolled and chit value correspondence
+	int DiceRollTotal = Dice1+Dice2;
+
+	cout << "Player recieves " << chits[DiceRollTotal-1] <<". " << endl;
+	playerOne.drawResource(&ResourceDeck, chits[DiceRollTotal-1], 1);
+
+	// Display the current status of the resource bank
+	ResourceDeck.DisplayResourceBankStatus();
+
+	////openingLayout();
+	////comeoutTurn();
+	//while (winner == NULL)
+	//{
+	//	//doNextTurn();
+	//}
 }
 
 void OpeningMenu(int *numOfPlayers)
@@ -44,6 +65,7 @@ void OpeningMenu(int *numOfPlayers)
 	cout << "		Settlers of Catan		"<< endl << endl
 		 << "Number of players?: ";
 	cin >> *numOfPlayers;
+	cout << endl;
 }
 
 void AssignChitResources(char *chits)
@@ -69,4 +91,9 @@ void DebugOutputResourceChits(char *chits)
 		cout << chits[x] << endl;
 	}
 }
-*/
+
+void RollDice(int *Dice1, int *Dice2)
+{
+	*Dice1 = rand()%6+1;
+	*Dice2 = rand()%6+1;
+}

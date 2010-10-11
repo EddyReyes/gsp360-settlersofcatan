7642c3092b990063c249b6ebd86046b35aa8f405
anarkin002 <anarkin002@d3192985-ef62-ff69-2b9f-3971aa3a1032>
2010-10-11 02:28:13 +0000

    Made 3-d Dice with paint and photoshop shaders polishing...working on SDL_MUSIC to make the rest of sounds

--- trunk/SettlersOfCatanMainProject/SettlersOfCatanGame/SDL_music.cpp
+++ trunk/SettlersOfCatanMainProject/SettlersOfCatanGame/SDL_music.cpp
@@ -7,13 +7,23 @@ Music::Music(){}
 Music::~Music(){}
 
 Mix_Chunk *_dice;
+//Mix_Chunk *_music;
+//Mix_Chunk *_dice;
+//Mix_Chunk *_dice;
 
-void Music::playWAV()
+
+void Music::play_diceWAV()
 { 
     Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
     _dice = Mix_LoadWAV("dice_sound.wav");
-   Mix_PlayChannelTimed( -1, _dice, 0, -1); //    Mix_PlayChannelTimed( -1, _dice, 1, -1);
+   Mix_PlayChannel( -1, _dice, 0); //    Mix_PlayChannelTimed( -1, _dice, 1, -1);
 }
+//void Music::play_musicWAV()
+//{ 
+//    Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
+//    _music = Mix_LoadWAV("music_sound.wav");
+//   Mix_PlayChannel( -1, _music, 0);
+//}
 
 void Music::stopMusic()
 {
@@ -21,7 +31,7 @@ void Music::stopMusic()
 	Mix_FreeChunk( _dice); 
 	//Mix_FreeChunk( _cheer); 
 	//Mix_FreeChunk( _beat); 
-	//Quit SDL_mixer 
+	/*Quit SDL_mixer;*/
 	Mix_CloseAudio(); 
 }
 
